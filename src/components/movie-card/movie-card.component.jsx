d306811fcd25e3ff6a79538ff70a9b41ef72f955
leo_liu <leo_liu@raytrex.com>
2024-07-21 14:37:55 +0800

    update movieCardList style

--- src/components/movie-card/movie-card.component.jsx
+++ src/components/movie-card/movie-card.component.jsx
@@ -7,10 +7,10 @@ const MovieCard = ({movie, button, showRating}) => {
     return (
         <div className=" relative w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href={`/movies/${movie.id}`}>
-                <img className="p-4 rounded-t-lg" src={`${getMovieImg('500', movie.backdrop_path)}`} alt="" />
+                <img className="p-4 rounded-t-lg" src={`${getMovieImg('500', movie.poster_path)}`} alt="" />
             </a>
 
-            {showRating && <div className=' w-1/4 absolute top-0 left-0'>
+            {showRating && <div className=' w-1/5 absolute top-0 left-[-1rem]'>
                 <CircularProgressbar value={votePercent.toFixed(1)} text={`${votePercent.toFixed(1)}%`} background
                     backgroundPadding={6}
                     styles={buildStyles({
@@ -25,8 +25,8 @@ const MovieCard = ({movie, button, showRating}) => {
             <div className="px-5 pb-5">
                 <div className=' h-20'>
                     <a href={`/movies/${movie.id}`}>
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
-
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900">{movie.title}</h5>
+                        <p className='text-gray-400'>{movie.release_date}</p>
                     </a>
                 </div>
                 {button(movie.id)}
